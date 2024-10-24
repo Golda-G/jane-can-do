409e2f5c4a46f9f5581784f83a64299fddb393c2
golda-g <goldagatsey@gmail.com>
2024-10-24 01:57:04 +0000

    q

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -8,7 +8,7 @@ const MiddleSection = () => {
   <div className="my-10 grid grid-cols-1 md:grid-rows-6 md:grid-cols-6 text-center text-black md:overflow-x-hidden">
 
     <div className="pt-2 md:relative md:col-start-1 md:col-span-3 md:row-start-1 md:row-span-3 md:ml-2"> 
-      <div className="hidden lg:block border border-gray-400 bg-gray-300 md:rounded-full md:p-16 md:relative md:z-20
+      <div className="border border-gray-400 bg-gray-300 md:rounded-full md:p-16 md:relative md:z-20
       hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <h6 className="leading-5">
             Jane Can Do offers a scalable and flexible directory for finding talent but with a unique focus on empowering women (inclusive)
@@ -18,7 +18,7 @@ const MiddleSection = () => {
                 </p> 
           </h6>
         </div>
-      <div className=" md:absolute md:translate-x-2 md:-translate-y-2 md:inset-0 sm:z-10 bg-gradient-to-br from-pink-500
+      <div className="hidden lg:block md:absolute md:translate-x-2 md:-translate-y-2 md:inset-0 md:z-10 bg-gradient-to-br from-pink-500
        via-violet-300 to-violet-600 rounded-full"></div>
     </div>
 
