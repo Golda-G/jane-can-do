479e25637cc3f2b37840eb8e52d2420681550e30
golda-g <goldagatsey@gmail.com>
2024-10-24 01:24:48 +0000

    media queries

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -5,10 +5,10 @@ const MiddleSection = () => {
   return (
     <section >
      
-  <div className="my-10 grid grid-cols-2 md:grid-rows-6 md:grid-cols-6  text-center text-black md:overflow-x-hidden">
+  <div className="my-10 grid grid-rows-2 md:grid-rows-6 md:grid-cols-6  text-center text-black md:overflow-x-hidden">
 
-    <div className="col-span-1 gap-1 pt-2 md:relative md:col-start-1 md:col-span-3 md:row-start-1 md:row-span-3 md:ml-2"> 
-      <div className="col-span-1 border border-gray-400 bg-gray-300 rounded-full p-16 md:relative md:z-20
+    <div className="row-span-1 gap-2 pt-2 md:relative md:col-start-1 md:col-span-3 md:row-start-1 md:row-span-3 md:ml-2"> 
+      <div className="col-span-1 border border-gray-400 bg-gray-300 rounded-full md:p-16 md:relative md:z-20
       hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <h6 className="leading-5">
             Jane Can Do offers a scalable and flexible directory for finding talent but with a unique focus on empowering women (inclusive)
