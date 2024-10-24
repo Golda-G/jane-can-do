4eff53d8dbe6e1d50a4840fd9008d46487ef47fa
golda-g <goldagatsey@gmail.com>
2024-10-24 01:44:28 +0000

    q

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -5,7 +5,7 @@ const MiddleSection = () => {
   return (
     <section >
      
-  <div className="my-10 grid grid-cols-2 md:grid-rows-6 md:grid-cols-6 text-center text-black md:overflow-x-hidden">
+  <div className="my-10 grid grid-cols-1 md:grid-rows-6 md:grid-cols-6 text-center text-black md:overflow-x-hidden">
 
     <div className="pt-2 md:relative md:col-start-1 md:col-span-3 md:row-start-1 md:row-span-3 md:ml-2"> 
       <div className="col-span-1 gap-2 border border-gray-400 bg-gray-300 md:rounded-full md:p-16 md:relative md:z-20
@@ -18,7 +18,7 @@ const MiddleSection = () => {
                 </p> 
           </h6>
         </div>
-      <div className="hidden md:flex md:absolute md:translate-x-2 md:-translate-y-2 md:inset-0 sm:z-10 bg-gradient-to-br from-pink-500
+      <div className="hidden lg:block md:absolute md:translate-x-2 md:-translate-y-2 md:inset-0 sm:z-10 bg-gradient-to-br from-pink-500
        via-violet-300 to-violet-600 rounded-full"></div>
     </div>
 
