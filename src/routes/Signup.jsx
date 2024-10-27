5ec133583a7a091ad6a8f78c4bd6a847745eda06
golda-g <goldagatsey@gmail.com>
2024-10-27 00:10:27 +0000

    authentication checksauthentication error

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import { auth } from '../firebase';
+import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
